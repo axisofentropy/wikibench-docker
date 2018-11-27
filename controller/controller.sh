85609e48ae1587bb405bbaf197386f34bb1c5142
Adam Vollrath <adam@endpoint.com>
2018-11-27 13:17:26 -0600

    Initial commit including Controller

---
+++ controller/controller.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cat ./traces | \
+	java -classpath . -jar $WIKIBENCH/tracebench/build/lib/TraceBench.jar 0 "jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DATABASE?user=$MYSQL_USER&password=$MYSQL_PASSWORD" plsampling epoch_ts | \
+       	sed 's:wiki/:index.php/:' | \
+	sed 's:w/::' | \
+	$WIKIBENCH/wikijector/wikijector.sh controller /app/$HTTPCORE/lib /app/$HTTPCLIENT/lib $HOSTNAME $WORKER_COUNT
