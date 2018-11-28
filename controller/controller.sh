f792b9e52ce6738f319869822ab317f4b9c71d09
Adam Vollrath <adam@endpoint.com>
2018-11-27 18:46:16 -0600

    Put those training wheels back on.

--- controller/controller.sh
+++ controller/controller.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+echo "Compiling traces"
 cat ./traces | \
 	java -classpath . -jar $WIKIBENCH/tracebench/build/lib/TraceBench.jar 0 "jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DATABASE?user=$MYSQL_USER&password=$MYSQL_PASSWORD" plsampling epoch_ts | \
        	sed 's:wiki/:index.php/:' | \
 	sed 's:w/::' > ./tracebench
+echo "Starting controller"
 cat ./tracebench | $WIKIBENCH/wikijector/wikijector.sh controller /app/$HTTPCORE/lib /app/$HTTPCLIENT/lib $HOSTNAME $WORKER_COUNT
