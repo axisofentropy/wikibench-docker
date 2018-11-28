e0f44a7811c4f752b52075cfabfd2fbbd3f8fa8a
Adam Vollrath <adam@endpoint.com>
2018-11-27 18:21:44 -0600

    Write TraceBench to disk instead of piping.

--- controller/controller.sh
+++ controller/controller.sh
@@ -2,5 +2,5 @@
 cat ./traces | \
 	java -classpath . -jar $WIKIBENCH/tracebench/build/lib/TraceBench.jar 0 "jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_DATABASE?user=$MYSQL_USER&password=$MYSQL_PASSWORD" plsampling epoch_ts | \
        	sed 's:wiki/:index.php/:' | \
-	sed 's:w/::' | \
-	$WIKIBENCH/wikijector/wikijector.sh controller /app/$HTTPCORE/lib /app/$HTTPCLIENT/lib $HOSTNAME $WORKER_COUNT
+	sed 's:w/::' > ./tracebench
+cat ./tracebench | $WIKIBENCH/wikijector/wikijector.sh controller /app/$HTTPCORE/lib /app/$HTTPCLIENT/lib $HOSTNAME $WORKER_COUNT
